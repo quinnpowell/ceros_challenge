f5dfddcb90342e8a1b06c1706bdebca15a229810
Quinn Powell <quinn.powell@gmail.com>
2022-01-12 00:05:47 +0100

    Overrides the goTo() method in SwaglabsLoginPage to not call pageLoaded() since it does not exist

--- pages/swagLabsLoginPage.js
+++ pages/swagLabsLoginPage.js
@@ -5,5 +5,10 @@ class SwaglabsLoginPage extends BasePage {
         super();
         this.url = "https://www.saucedemo.com";
     }
+
+    async goto() {
+        await browser.get(this.url, this.timeout.xl);
+        expect(await browser.getCurrentUrl()).toContain(this.url);
+    }
 }
 export default new SwaglabsLoginPage();
